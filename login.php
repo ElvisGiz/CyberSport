f13f905ee10ed0f7355b601a4ad091b85b2ef7d2
Elvis <yodamorfius@gmail.com>
2022-05-05 23:09:02 +0500

    fix db query

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
 require "header.php";
 
 
-$link = mysqli_connect("localhost", "mysql", "");
+$link = mysqli_connect("localhost", "mysql", "", "cyber");
 
 if ($link == false) {
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
 } else {
     mysqli_set_charset($link, "utf8");
-    $sql = 'INSERT INTO user SET name="1"';
+    $sql = 'INSERT INTO user ("name") VALUES ("test")';
     $result = mysqli_query($link, $sql);
 
     if ($result == false) {
